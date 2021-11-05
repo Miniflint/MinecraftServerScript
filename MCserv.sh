c33d18959f0a9eaa952b95b3d9e0cf9faa31154f
Miniflint <79022357+Miniflint@users.noreply.github.com>
2021-11-05 20:48:04 +0100

    Updated MCserv.sh

--- MCserv.sh
+++ MCserv.sh
@@ -10,6 +10,8 @@ echo -e "${green}Enter The name of your folder  ex : ${NC}PaperMC ${NC}/ ${RED}F
 read DirName
 echo -e "${green}Now please enter the url of the file to download ex : ${NC}https.../.../.../.../file.jar: ${NC}"
 read Url
+echo -e "${green}Enter the gamemode -> ex : survival / hardcore / creative"
+read Gamemode
 Name="${DirName}.jar"
 DirPath="/opt/minecraft${DirName}"
 PathJar="/opt/minecraft${DirName}/${Name}"
@@ -24,7 +26,7 @@ test $? -eq 0 || exit 1 "You should have sudo priveledge to run this script"
 echo -e "\n${green}Installing the must-have pre-requisites${NC}${cyan}"
 while read -r p ; do sudo apt-get install -y $p ; done < <(cat << "EOF"
     net-tools
-    default-jre-headless
+    openjdk-16-jdk
     curl
     screen
 EOF
@@ -74,13 +76,13 @@ sed -i 's/eula=false/eula=true/' eula.txt
 echo -e "${NC}Accepting EULA TERM : [${green}OK${NC}]"
 
 echo -e "\n\n${green}Writing in server.properties${NC}"
-ServerProperties=$"#Minecraft server properties\n#(last boot timestamp)\nenable-jmx-monitoring=false\nrcon.port=25575\nlevel-seed=\gamemode=survival\nenable-command-block=false\nenable-query=false\ngenerator-settings=\nlevel-name=world\n\nquery.port=25565\npvp=true\ngenerate-structures=true\ndifficulty=easy\nnetwork-compression-threshold=256\nmax-tick-time=60000\nmax-players=15\nuse-native-transport=true\nonline-mode=true\nenable-status=true\nallow-flight=false\nbroadcast-rcon-to-ops=true\nview-distance=10\nmax-build-height=256\nserver-ip=\nallow-nether=true\nserver-port=25565\nenable-rcon=false\nsync-chunk-writes=true\nop-permission-level=4\nprevent-proxy-connections=false\nresource-pack=\nentity-broadcast-range-percentage=100\nrcon.password=\nplayer-idle-timeout=0\nforce-gamemode=false\nrate-limit=0\nhardcore=false\nwhite-list=true\nbroadcast-console-to-ops=true\nspawn-npcs=true\nspawn-animals=true\nsnooper-enabled=true\nfunction-permission-level=2\nlevel-type=default\nspawn-monsters=true\nenforce-whitelist=false\nresource-pack-sha1=\nspawn-protection=16\nmax-world-size=29999984\nmotd=SERVER"
+ServerProperties=$"#Minecraft server properties\n#(last boot timestamp)\nenable-jmx-monitoring=false\nrcon.port=25575\nlevel-seed=\ngamemode=${Gamemode}\nenable-command-block=false\nenable-query=false\ngenerator-settings=\nlevel-name=world\nquery.port=25565\npvp=true\ngenerate-structures=true\ndifficulty=hard\nnetwork-compression-threshold=256\nmax-tick-time=60000\nmax-players=15\nuse-native-transport=true\nonline-mode=true\nenable-status=true\nallow-flight=false\nbroadcast-rcon-to-ops=true\nview-distance=10\nmax-build-height=256\nserver-ip=\nallow-nether=true\nserver-port=25565\nenable-rcon=false\nsync-chunk-writes=true\nop-permission-level=4\nprevent-proxy-connections=false\nresource-pack=\nentity-broadcast-range-percentage=100\nrcon.password=\nplayer-idle-timeout=0\nforce-gamemode=false\nrate-limit=0\nhardcore=false\nwhite-list=true\nbroadcast-console-to-ops=true\nspawn-npcs=true\nspawn-animals=true\nsnooper-enabled=true\nfunction-permission-level=2\nlevel-type=default\nspawn-monsters=true\nenforce-whitelist=false\nresource-pack-sha1=\nspawn-protection=16\nmax-world-size=29999984\nmotd=SERVER"
 echo -e ${ServerProperties} > server.properties
 [ ! -s server.properties ] && echo -e "${NC}Server.properties overwrite: [${RED}NOT OK${NC}]" && exit || echo -e "${NC}Server.properties overwrite: [${green}OK${NC}]"
 
 echo -e "$\n{green}Creating the script folder and start the minecraft server${NC}"
 [ ! -d /opt/scripts ] && echo -e "${green}Directory 'script' don't exist : creating one" && cd /opt mkdir scripts && sleep 3
-        [ ! -d /opt/scripts ] && echo -e "${NC}Creation of the directory script: [${RED}NOT OK${NC}]" && exit || echo -e "${NC}Creation of the directory script: [${green}OK${NC>
+        [ ! -d /opt/scripts ] && echo -e "${NC}Creation of the directory script: [${RED}NOT OK${NC}]" && exit || echo -e "${NC}Creation of the directory script: [${green}OK${NC}]"
 
 cd /opt/scripts
 bin=$"#!/bin/bash\n\t"
@@ -104,6 +106,10 @@ case $input in
  echo -e "\n\nThanks for downloading :)"
  sleep 3
        ;;
+    *)
+ echo "Invalid input"
+ exit 1
+ ;;
 esac
 echo -e "\n${green}Configuring the start of the server"
 screen -S ${DirName} && cd ${DirPath} && ${Startup}
