39938122378343b8e822c01da3a3f41086d02c94
root <Tristan.goel@gmail.com>
2021-12-03 02:08:19 +0000

    Clarification of variables, added verifications

--- MCserv.sh
+++ MCserv.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #variable time
-NC='\033[0m' # No Color
+no_color='\033[0m' # No Color
 green=`tput setaf 2`
-RED='\033[0;31m'
+red='\033[0;31m'
 cyan='\033[0;36m'
 
 #clear screen
@@ -14,21 +14,21 @@ printf "\033c"
 
 #Get input on :
 #1. Folder name
-#2. Url
+#2. url
 #3. Game mode
-echo -e "${green}Enter The name of your folder  ex : ${NC}PaperMC ${NC}/ ${RED}ForgeServer: ${NC}"
-read DirName
-echo -e "${green}Now please enter the url of the file to download ex : ${NC}https.../.../.../.../file.jar: ${NC}"
-read Url
-echo -e "${green}Enter the gamemode -> ex : ${NC}survival ${RED}/ ${NC}hardcore ${RED}/ ${NC}creative${NC}"
-read Gamemode
+echo -e "${green}Enter The name of your folder  ex : ${cyan}Vanilla ${no_color}/ PaperMC / ${red}ForgeServer: ${no_color}"
+read dir_name
+echo -e "${green}Now please enter the url of the file to download ex : ${no_color}https.../.../.../.../file.jar: ${no_color}"
+read url
+echo -e "${green}Enter the gamemode -> ex : ${no_color}survival ${red}/ ${no_color}hardcore ${red}/ ${no_color}creative${no_color}"
+read game_mode
 
 #Declare variables
-Name="${DirName}.jar"
-DirPath="/opt/minecraft${DirName}"
-PathJar="/opt/minecraft${DirName}/${Name}"
-Startup="java -Xms4G -Xmx5G -jar ${PathJar} nogui "
-scriptPath="/opt/scripts"
+name="${dir_name}.jar"
+dir_path="/opt/minecraft${dir_name}"
+path_jar="/opt/minecraft${dir_name}/${name}"
+startup="java -Xms4G -Xmx5G -jar ${path_jar} nogui "
+script_path="/opt/scripts"
 
 set -eu -o pipefail # fail on error , debug all lines
 
@@ -38,10 +38,11 @@ test $? -eq 0 || exit 1 "You should have sudo priveledge to run this script"
 
 #fonction to print if it's OK or NOT OK
 check_if_ok () {
-	not_ok="${NC}[${RED}NOT OK${NC}] : $2"
+	not_ok="${no_color}[${red}NOT OK${no_color}] : $2"
+	ok="${no_color}[${green}OK${no_color}] : $2"
 	if [[ $1 == 1 ]]
 	then
-		echo -e "${NC}[${green}OK${NC}] : $2"
+		echo -e ${ok}
 	elif [[ $1 == 0 ]]
 	then
 		echo -e ${not_ok}
@@ -51,10 +52,11 @@ check_if_ok () {
 	fi
 }
 
-if [[ -d $DirPath ]]
+if [[ -d $dir_path ]]
 then
 	check_if_ok 0 "This folder already exists"
 fi
+
 #install requirement. you may need to use another version of open-JDK
 #you may also need multiple java version
 #you can add a package by doing this : package+=("package_name_here")
@@ -64,36 +66,37 @@ fi
 ############## NET TOOLS CAN BE DELETED FROM REQUIREMENT, BUT PROBLEMS MAY COME #
 ############## CURL IS ESSENTIAL ALONG WITH SCREEN ##############################
 ############## THE JAVA VERSION CAN BE MODIFIED #################################
+
 package=("net-tools" "curl" "screen")
+
 #java version
 package+=("openjdk-16-jdk")
 
 for packages in "${packages[@]}"; do
   sudo apt-get install -y "$packages" &> /dev/null
 done
-
 sleep 1
+
 #check pre-requisite
 for packages in ${package[@]}
 do
 	if [[ `apt-cache search --names-only "$packages"` ]]
 	then
-		check_if_ok 1 "installation of pre-requisite : $packages"
+		check_if_ok 1 "installation of pre-requisite -> $packages"
 	else
-		check_if_ok 2 "installation of pre-requisite : $packages"
+		check_if_ok 2 "installation of pre-requisite -> $packages"
 		#try resintallation if fail on first try
 		sudo apt-get install -y "$packages" &> /dev/null
 		sleep 1
 		if [[ `apt-cache search --names-only "$packages"` ]]
 		then
-			check_if_ok 1 "installation of pre-requisite : $packages"
+			check_if_ok 1 "re-installation of pre-requisite -> $packages"
 		else
 			#exit if fail the second time
-			check_if_ok 0 "installation of pre-requisite : $packages"
+			check_if_ok 0 "re-installation of pre-requisite -> $packages"
 		fi
 	fi
 done
-
 sleep 2
 
 #Check for the /opt directory (should exists by default. stand for "optional")
@@ -103,12 +106,19 @@ then
 else
 	check_if_ok 2 "Checking /opt"
 	mkdir "/opt"
+	sleep 2
+	if [[ -d /opt ]]
+	then
+		check_if_ok 1 "Checking /opt"
+	else
+		check_if_ok 0 "Checking /opt"
+	fi
 fi
 
 #make the directory that will have all the files
-mkdir ${DirPath}
+mkdir ${dir_path}
 sleep 2
-if [[ -d ${DirPath} ]]
+if [[ -d ${dir_path} ]]
 then
 	check_if_ok 1 "Folder Creation"
 else
@@ -116,15 +126,15 @@ else
 fi
 
 #check if the script path already exists
-if [[ -d ${scriptPath} ]]
+if [[ -d ${script_path} ]]
 then
         check_if_ok 1 "'Scripts' folder"
 else
         check_if_ok 2 "'Scripts' folder"
 	#on fail, try to do it again
-        mkdir ${scriptPath}
-        sleep 3
-        if [[ -d ${scriptPath} ]]
+        mkdir ${script_path}
+        sleep 2
+        if [[ -d ${script_path} ]]
         then
                 check_if_ok 1 "Creation of the 'scripts' folder"
         else
@@ -135,17 +145,16 @@ fi
 
 #check for the server.properties/gamemode file
 bin=$"#!/bin/bash\n\t"
-if [[ -f "server.properties/$Gamemode.txt" ]]
+if [[ -f "server.properties/$game_mode.txt" ]]
 then
-	ServerProperties=$(cat server.properties/${Gamemode}.txt)
         check_if_ok 1 "Checking file"
 else
         check_if_ok 0 "Checking file"
 fi
 
 #downloading url
-curl -o ${PathJar} ${Url} --silent
-if [[ -f ${PathJar} ]]
+curl -o ${path_jar} ${url} --silent
+if [[ -f ${path_jar} ]]
 then
 	check_if_ok 1 "Download URL"
 else
@@ -155,26 +164,26 @@ fi
 
 ############### IF URL IS FORGE STARTING IS DIFFERENT #########
 ############### I DON'T KNOW IF IT STILL WORKS ################
-cd ${DirPath}
-if [[ $Url == *"forge"* ]]
+cd ${dir_path}
+if [[ $url == *"forge"* ]]
 then
-	java -jar ${PathJar} --installServer &> /dev/null
+	java -jar ${path_jar} --installServer &> /dev/null
 else
-	java -jar ${PathJar} &> /dev/null 
+	java -jar ${path_jar} &> /dev/null 
 fi
 check_if_ok 1 "Un-jaring the file"
 
-if [[ $Url == *"forge"* ]]
+if [[ $url == *"forge"* ]]
 then
-	var=$(/bin/find ${DirPath} -maxdepth 1 -name "forge-1.*.jar")
-	echo -e "${RED}${var}\n${DirPath}"
-	cd ${DirPath} && java  -Xms1024M -Xmx2000M -jar ${var} nogui #    cd /opt/minecraft && java -Xms1024M -Xmx2000M -jar /opt/minecraft/forge-1.12.2-14.23.5.2854.jar nogui 
+	var=$(/bin/find ${dir_path} -maxdepth 1 -name "forge-1.*.jar")
+	echo -e "${red}${var}\n${dir_path}"
+	cd ${dir_path} && java  -Xms1024M -Xmx2000M -jar ${var} nogui #    cd /opt/minecraft && java -Xms1024M -Xmx2000M -jar /opt/minecraft/forge-1.12.2-14.23.5.2854.jar nogui 
 else
-	${Startup} &> /dev/null
+	${startup} &> /dev/null
 fi
 
 #Checking server.properties file
-if [[ -f "${DirPath}/server.properties" ]]
+if [[ -f "${dir_path}/server.properties" ]]
 then
 	check_if_ok 1 "Starting the Server File"
 else
@@ -187,6 +196,7 @@ check_if_ok 1 "Acceptiing EULA terms"
 
 
 #writing server.properties in server.properties
+cp "server.properties/$game_mode.txt" "${dir_path}/server.properties" 
 echo "${ServerProperties}" > server.properties
 if [[ -s server.properties ]]
 then
@@ -197,17 +207,17 @@ fi
 
 #writing the script
 bin=$"#!/bin/bash\n\t"
-echo -e "${bin} cd ${DirPath} && ${Startup}" > "${scriptPath}/${DirName}.sh"
-chmod +x ${scriptPath}/${DirName}.sh
+echo -e "${bin} cd ${dir_path} && ${startup}" > "${script_path}/${dir_name}.sh"
+chmod +x ${script_path}/${dir_name}.sh
 sleep 2
 
-if [[ -f ${scriptPath}/${DirName}.sh ]]
+if [[ -f ${script_path}/${dir_name}.sh ]]
 then
 	check_if_ok 1 "Creation of the script"
 else
 	check_if_ok 2 "Creation of the script"
-	echo -e "${bin} cd ${DirPath} && ${Startup}" > "${scriptPath}/${DirName}.sh"
-	if [[ -f ${scriptPath}/${DirName}.sh ]]
+	echo -e "${bin} cd ${dir_path} && ${startup}" > "${script_path}/${dir_name}.sh"
+	if [[ -f ${script_path}/${dir_name}.sh ]]
 	then
         	check_if_ok 1 "Creation of the script"
 	else
@@ -218,6 +228,6 @@ else
 fi
 
 sleep 2
-cd ${scriptPath}
+cd ${script_path}
 check_if_ok 1 "Having fun"
-echo -e "${cyan}THANKS FOR DOWNLOADING${NC}"
+echo -e "${cyan}THANKS FOR DOWNLOADING${no_color}"
