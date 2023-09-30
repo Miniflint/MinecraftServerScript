b9571e72f63ef8c1f674a94d97d6b148759aae50
root <Tristan.goel@gmail.com>
2023-09-30 06:27:52 +0000

    Upgraded python script

--- BackupTime.py
+++ BackupTime.py
@@ -9,12 +9,19 @@ NC	= '\033[0m'
 RED	= '\033[0;31m'
 CYAN	= '\033[0;36m'
 GREEN	= '\033[0;32m'
+YELLOW	= '\033[0;33m'
+
+def print_loading(output:str) -> None:
+	print(f"[{YELLOW}STARTING{NC}]: {YELLOW}{output}{NC}")
 
 def print_color(output:str):
 	print(f"[{GREEN}OK{NC}]: {CYAN}{output}{NC}")
 
+def print_not_ok(output:str) -> None:
+	print(f"[{RED}NOT OK{NC}]: {RED}{output}{NC}")
+
 def handle_error(error_str:str, exit_code:int) -> None:	
-	print(f"[{RED}NOT OK{NC}]: {RED}{error_str}{NC}")
+	print_not_ok(error_str)
 	exit(exit_code)
 
 def check_exist_dir(pathdir:str) -> bool:
@@ -24,9 +31,10 @@ def check_exist_dir(pathdir:str) -> bool:
 
 def check_backup_done(backup_fullpath) -> None:
 	sleep(1)
-	if not check_exist_dir(backup_fullpath):
-		handle_error(f"Couldn't find {backup_fullpath} file")
-	print_color("Archive has been created")
+	if check_exist_dir(backup_fullpath):
+		print_color(f"Archive '{backup_fullpath}' has been created")
+		return
+	handle_error(f"Couldn't find {backup_fullpath} file")
 
 def display_available_dir() -> None:
 	dirs = os.listdir(BASE_DIR)
@@ -34,21 +42,43 @@ def display_available_dir() -> None:
 		print(BASE_DIR + directory)
 
 def questions():
-	PathToBackup = input(f"{GREEN}Enter the full path of the folder to backup\n{CYAN}ex: {NC}/opt/minecraftPaperMC\n")
+	while True:
+		try:
+			backup_check = int(input(
+f'''
+{GREEN}Would you like to backup:
+	1. all files(longer, more complete)
+	2. Or only required ones (faster, less disk used){NC}
+Enter your number: ''') or 2)
+		except ValueError:
+			print_not_ok("please enter a number")
+			continue
+		if backup_check <= 2 and backup_check >= 1:
+			break
+		else:
+			print_not_ok("please enter a number between 1 and 2")
+	state = "Backup all files" if backup_check == 1 else "Backup only required files"
+	PathToBackup = input(f"{GREEN}Enter the full path of the folder to backup\n{CYAN}ex: {NC}/opt/minecraftPaperMCServer\n") or "/opt/minecraftPaperMCServer"
 	if not check_exist_dir(PathToBackup):
 		handle_error("Couldn't find the minecraft folder path", 127)
-	backup_path_dir = input(f"{GREEN}Enter the backup path\n{CYAN}ex: {NC}/home/ubuntu/Backup\n")
+	backup_path_dir = input(f"{GREEN}Enter the backup path\n{CYAN}ex: {NC}/home/ubuntu/Backup\n") or "/home/ubuntu/Backup"
 	if not check_exist_dir(backup_path_dir):
 		handle_error("Couldn't find the backup path", 127)
-	backup_name = input(f"{GREEN}Enter the backup Name\n{CYAN}ex: {NC}Backup- \n")
+	backup_name = input(f"{GREEN}Enter the backup Name\n{CYAN}ex: {NC}Backup- \n") or "Backup-1"
 	if check_exist_dir(backup_name):
 		handle_error(f"Backup already exist", 1)
+	print_color(state)
+	print_loading("Checking if minecraft folder")
 	print_color(PathToBackup)
+	print_loading("Checking if backup path exist")
 	print_color(backup_path_dir)
+	print_loading("Checking if backup archive exist")
 	print_color(backup_name)
 	if not PathToBackup.endswith("/"):
 		PathToBackup = PathToBackup + "/"
-	return PathToBackup, backup_path_dir, backup_name
+	if not backup_path_dir.endswith("/"):
+		backup_path_dir = backup_path_dir + "/"
+	return backup_check, PathToBackup, backup_path_dir, backup_name
 
 def get_required_dir_file(path_dir:str):
 	#get jar executable file
@@ -84,19 +114,27 @@ def get_required_dir_file(path_dir:str):
 	return " ".join(checking_dirs)
 
 def exec_tar(backup_dir:str, backup_name:str, folders_to_backup:str) -> bool:
-	backup_fullpath = backup_dir + "/" + backup_name + "-" + str(NOW) + "tar.gz"
+	backup_fullpath = backup_dir + backup_name + "-" + str(NOW) + "tar.gz"
 	command = f"tar -czvf {backup_fullpath} {folders_to_backup}"
 	hide_output = " > /dev/null 2>&1"
-	os.system(command + hide_output)
-	check_backup_done(backup_fullpath)
+	print_loading("Starting archive")
+	try:
+		os.system(command + hide_output)
+	finally:
+		check_backup_done(backup_fullpath)
 
-def main():
+def main() -> int:
 	print("\033c", end="")
 	display_available_dir()
 	print("\n")
-	path_dir, backup_dir, backup_name = questions()
-	files_to_tar = get_required_dir_file(path_dir)
+	check, path_dir, backup_dir, backup_name = questions()
+	if check == 2:
+		files_to_tar = get_required_dir_file(path_dir)
+	else:
+		files_to_tar = path_dir
 	exec_tar(backup_dir, backup_name, files_to_tar)
+	print_color("THANKS FOR USING THIS PROGRAM")
+	return (0)
 
 if __name__ == "__main__":
 	try:
